b60c0cd1e3365b9495ed995a90ee5b63dd4e9710
Dustin Matthew Burt <dmburt@berkeley.edu>
2025-03-30 17:31:14 -0400

    Updated core for imports

--- bistro/core.py
+++ bistro/core.py
@@ -1,6 +1,37 @@
 
 __version__ = '1.0.20221230'
 
+# data libraries
+import pandas as pd
+import numpy as np
+import patsy as pt
+import xarray as xr
+
+# stats libraries
+#import pymc3 as pm
+import pymc as pm
+import bambi as bmb
+import arviz as az
+
+# control group matching libraries
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+# viz libraries
+# Note: graphviz will require additional binaries to be installed
+import graphviz
+import seaborn as sns
+from tabulate import tabulate
+
+from matplotlib import pyplot as plt
+from matplotlib.ticker import PercentFormatter
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rcParams
+
+
+
+
 class BModel:
 
     def _convert_dataset(self, dataset, dataset_name):
